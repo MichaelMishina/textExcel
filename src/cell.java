09614f1b5c57c7ae76b55e85df99509fc387d32c
ArgonStratos <qrawson@gmail.com>
2016-03-28 08:36:04 -0700

    started clear
    not done a lot yet but i started clear

--- src/cell.java
+++ src/cell.java
@@ -2,5 +2,11 @@
  * Created by bal_mcmishina on 3/8/2016.
  */
 public class cell {
+    private String emptyCell = "            ";
+    public cell(char row, int col){}
 
+//set cell as 12 spaces
+    public void clearCell (char row, int col){
+         cell[row][col] = emptyCell;
+    }
 }
--- src/textCell.java
+++ src/textCell.java
@@ -2,4 +2,5 @@
  * Created by bal_mcmishina on 3/8/2016.
  */
 public class textCell extends cell {
+
 }
