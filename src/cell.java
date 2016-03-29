6a8cd52041f9feb270ac6e7b5d0ee0f5f6bfb1ae
ArgonStratos <qrawson@gmail.com>
2016-03-29 08:08:19 -0700

    combined our code for cell
    ^^

--- src/cell.java
+++ src/cell.java
@@ -1,26 +1,21 @@
 /**
  * Created by bal_mcmishina on 3/8/2016.
  */
-<<<<<<< HEAD
+
 public class cell {
+
+    private int row;
+    private int col;
+    boolean isEmpty;
     private String emptyCell = "            ";
     public cell(char row, int col){}
 
 //set cell as 12 spaces
     public void clearCell (char row, int col){
          cell[row][col] = emptyCell;
-=======
-public class Cell {
 
-    private int row;
-    private int col;
-    boolean isEmpty;
-    public Cell(){
-        
+
     }
 
-    public String get(char row, char col){
-     return("temporary");
->>>>>>> origin/master
     }
 }
