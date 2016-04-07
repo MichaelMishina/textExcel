d0351c4afcb9f55233c42291d1d692cd8242bed4
ArgonStratos <qrawson@gmail.com>
2016-04-07 16:23:39 -0700

    Mr. V got print working, i watched.

--- src/SpreadSheet.java
+++ src/SpreadSheet.java
@@ -16,7 +16,7 @@ public class SpreadSheet {
     //Instance variables
     private Scanner sheetScanner;
     private Cell[][] spreadSheetCells;
-    public boolean finisher = false;
+
 
     public SpreadSheet(){
         spreadSheetCells = new Cell[ROWCOUNT][COLCOUNT];
@@ -55,25 +55,18 @@ public class SpreadSheet {
         return out;
     }
 
-    public void commandInput(Scanner commandScanner){
-        System.out.print("Welcome. ");
-        while(!finisher) {
-            System.out.println("Enter a command.");
-            String input1 = commandScanner.nextLine().toUpperCase();
-            int char1 = (int)input1.charAt(0) - (int)'A';
-            if ((input1.toLowerCase().contains("print"))) {
-               System.out.println(printSheet() );
+    public String printCellValue(String input) {
 
-            } else if ((input1.toLowerCase().contains("exit"))) {
-                System.out.println("Process: end");
-                finisher = true;
-            }else if((char1 >= 0 && char1 <= (COLCOUNT - 1) ) && (Integer.parseInt(input1.substring(1) ) >= 1 &&
-                    (Integer.parseInt(input1.substring(1) ) ) <= ROWCOUNT) ) {
-                System.out.println(spreadSheetCells[char1][Integer.parseInt(input1.substring(1))]);
-            }
+        String cellValue = "Invalid input";
+        int col = (int) input.charAt(0) - (int) 'A';
+        int row = (Integer.parseInt(input.substring(1) ) - 1);
 
+        if ( ( (col >= 0) && (col < COLCOUNT) ) &&
+                ( (row >= 0) && (row < ROWCOUNT) ) ) {
+            cellValue = (input + " = " + spreadSheetCells[row][col]);
         }
 
+        return cellValue;
     }
 
     public Cell getCell(char col, int row){
--- src/TextExcelMain.java
+++ src/TextExcelMain.java
@@ -6,16 +6,38 @@ import java.util.Scanner;
 public class TextExcelMain {
 
     public static void main(String[]args){
-
+        boolean finisher = false;
         Scanner commandScanner = new Scanner(System.in);
         SpreadSheet newSheet = new SpreadSheet();
-        newSheet.commandInput(commandScanner);
 
+        System.out.print("Welcome. ");
+        while(!finisher) {
+            System.out.println("Enter a command.");
+            String input = commandScanner.nextLine().toUpperCase();
+
+            finisher = commandInput(input, newSheet);
+        }
 
 
 
     }
 
+    public static boolean commandInput(String input, SpreadSheet sheet){
+            boolean finisher = false;
 
+
+            if ((input.toLowerCase().contains("print"))) {
+                System.out.println(sheet.printSheet() );
+
+            } else if ((input.toLowerCase().contains("exit"))) {
+                System.out.println("Process: end");
+                finisher = true;
+            }else {
+                System.out.println(sheet.printCellValue(input));
+            }
+
+        return finisher;
+
+    }
 
 }
