04dda16c84824251c4e0d3872400935596776d9a
MichaelMishina <meestermnm@outlook.com>
2016-05-11 08:11:30 -0700

    Updated Spreadsheet class; Added in the GetCell method.

--- src/FormulaCell.java
+++ src/FormulaCell.java
@@ -3,16 +3,27 @@
  */
 
 public class FormulaCell extends Cell {
-    public String formula;
+    private String formula;
+    private SpreadSheet usedSheet;
 
     public FormulaCell(String originalData) {
         super(originalData);
         formula = originalData;
     }
 
-    //this is very wrong
-    public String sum(){
-        return("thing");
+    public String sum(int startCol, int endCol, int startRow, int endRow ){
+        double endInt = 0;
+        for (int k = startCol; k < endCol; k++) {
+            for (int m = startRow; m < endRow; m++) {
+                if(usedSheet.getCell(k,m).equals("")){
+                    m++;
+                } else {
+                    endInt += 1;
+                    //incomplete
+                }
+            }
+        }
+        return("incomplete");
     }
 
 
@@ -22,8 +33,8 @@ public class FormulaCell extends Cell {
     public String toSpreadsheet() {
         String[] split = formula.split(" ");
         //split[2] is used b/c it skips over the first parenthesis
-        double result = Double.parseDouble(split[1]);
-        for (int k = 2; k < split.length - 1; k++) {
+        double result = Double.parseDouble(split[2]);
+        for (int k = 3; k < split.length - 1; k++) {
             if (split[k].contains("+")) {
                 result += (Double.parseDouble(split[k + 1]));
             } else if (split[k].contains("-")) {
