421a0b0c5a864852dfdc8cb6c0c54c263a75373f
ArgonStratos <qrawson@gmail.com>
2016-05-31 14:27:16 -0700

    Merged everything again

--- src/NumberCell.java
+++ src/NumberCell.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by bal_mcmishina on 3/8/2016.
  */
@@ -21,19 +23,26 @@ public class NumberCell extends Cell{
     }
 
 
-    public void sort(int startCol, int endCol, int startRow, int endRow, boolean ascend ) {
+    public void sort(int startCol, int endCol, int startRow, int endRow ) {
+        ArrayList<NumberCell> tempArray = new ArrayList<>();
+        ArrayList<Cell> tempArray2 = new ArrayList<>();
+        int tempInt = 0;
         for (int k = startCol; k <= endCol; k++) {
             for (int m = startRow; m <= endRow; m++) {
-                if((usedSheet.getCell(k,m) instanceof NumberCell) && (usedSheet.getCell(k+1,m) instanceof NumberCell)){
-
-                } else if(usedSheet.getCell(k,m) instanceof FormulaCell){
-
+                if (usedSheet.getCell(k,m) instanceof NumberCell){
+                tempArray.add((NumberCell)(usedSheet.getCell(k,m)));
                 } else {
-                    m++;
+                tempArray2.add(usedSheet.getCell(k,m));
                 }
-
-
+                tempInt += 1;
+            }
+            for (int m = 0; m <= tempArray.size()){
+                if ((tempArray.get(m).getNumData()) > (tempArray.get((m+1)).getNumData())){
+                    double fac = tempArray.get(m).getNumData();
+                    tempArray.set(m, ) = tempArray.get(m+1).getNumData();
+                }
             }
         }
     }
 }
+
